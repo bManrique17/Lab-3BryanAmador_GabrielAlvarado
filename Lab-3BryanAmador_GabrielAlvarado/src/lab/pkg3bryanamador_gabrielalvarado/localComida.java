67329ec27060fb93731ddbf45fb5273f8de49a0f
Tu nombre aquí“$ git config --global user.name Manrique“ <bmanrique17@unitec.edu>
2017-10-27 14:20:33 -0600

    Clases creadas

---
+++ Lab-3BryanAmador_GabrielAlvarado/src/lab/pkg3bryanamador_gabrielalvarado/localComida.java
@@ -0,0 +1,50 @@
+
+package lab.pkg3bryanamador_gabrielalvarado;
+
+import java.util.ArrayList;
+
+public class localComida {
+    
+    ArrayList<Empleado> listEmpleados = new ArrayList();
+    ArrayList<Producto> listProductoAlimenticios = new ArrayList();
+    Producto ProcDia;
+
+    public localComida(Producto ProcDia) {
+        this.ProcDia = ProcDia;
+    }
+
+    public localComida() {
+    }
+
+    public ArrayList<Empleado> getListEmpleados() {
+        return listEmpleados;
+    }
+
+    public ArrayList<Producto> getListProductoAlimenticios() {
+        return listProductoAlimenticios;
+    }
+
+    public Producto getProcDia() {
+        return ProcDia;
+    }
+
+    public void setListEmpleados(ArrayList<Empleado> listEmpleados) {
+        this.listEmpleados = listEmpleados;
+    }
+
+    public void setListProductoAlimenticios(ArrayList<Producto> listProductoAlimenticios) {
+        this.listProductoAlimenticios = listProductoAlimenticios;
+    }
+
+    public void setProcDia(Producto ProcDia) {
+        this.ProcDia = ProcDia;
+    }
+
+    @Override
+    public String toString() {
+        return "localComida{" + "ProcDia=" + ProcDia + '}';
+    }
+    
+    
+    
+}
